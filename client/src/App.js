86d3c49651c9fb0f02cca280418c405f4049900e
Justin Johnson <jjustin634@gmail.com>
2020-03-21 00:20:42 -0500

    creating a route for creating a user account

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/navigation/navigation';
 import { HomePage } from './components/home/home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import { PrivateRoute } from './components/private_route/private_route';
+import Signup from './components/signup/signup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <PrivateRoute path="/" exact component={HomePage} />
           <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={Signup} />
         </Switch>
     </Router>
   );
