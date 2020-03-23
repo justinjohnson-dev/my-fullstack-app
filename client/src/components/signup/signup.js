4e435552525ff1b7cf74b8f4f10c76a963cd4cb1
Justin Johnson <jjustin634@gmail.com>
2020-03-22 19:55:33 -0500

    Adding client side logic for creating user account

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -1,70 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './signup.css';
+import axios from 'axios';
 import {Link} from 'react-router-dom';
+import { API } from '../../config';
 
 
 class Signup extends React.Component {
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: '',
-            name: '',
-            hasAgreed: false
-        };
+    this.state = {
+      name: null,
+      email: null,
+      password: null,
+    };
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  handleChange = (event) => {
+    event.preventDefault();
+    console.log(event)
+    console.log(event.target.name)
+    console.log(event.target.value)
+    this.setState({ 
+      [event.target.name]: event.target.value 
+    });
+  }
 
-    handleChange(e) {
-        let target = e.target;
-        let value = target.type === 'checkbox' ? target.checked : target.value;
-        let name = target.name;
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-        this.setState({
-          [name]: value
-        });
-    }
+    const { name, email, password } = this.state;
 
-    handleSubmit(e) {
-        e.preventDefault();
+    console.log(name)
+    const user = {
+      name,
+      email,
+      password,
+    };
 
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
-    }
+    fetch('http://localhost:5000/api/signup' , {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then((result) => result.json())
+    .then((info) => { console.log(info); })
+  };
 
-    render() {
-        return (
-        <div className="FormCenter">
-            <form onSubmit={this.handleSubmit} className="FormFields">
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="name">Full Name</label>
-                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
-              </div>
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
-              </div>
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </div>
-
-              <div className="FormField">
-                <label className="FormField__CheckboxLabel">
-                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" value={this.state.hasAgreed} onChange={this.handleChange} /> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                </label>
-              </div>
-
-              <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/sign-in" className="FormField__Link">I'm already member</Link>
-              </div>
-            </form>
-          </div>
-        );
-    }
+  render() {
+    const {name} = this.state 
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input onChange={this.handleChange} type="text" name='name' className="form-control" />
+        </div>
+        <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input onChange={this.handleChange} type="text" name='email' className="form-control" />
+        </div>
+        <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input onChange={this.handleChange} type="text" name='password' className="form-control" />
+        </div>
+        <button onChange={this.handleSubmit} className="button"><span>Submit</span></button>
+      </form>
+    );
+  }
 }
 
 export default Signup;
