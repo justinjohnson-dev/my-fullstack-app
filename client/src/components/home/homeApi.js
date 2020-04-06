d2a271e01baa1dae529f2d37e64e0c1eab0ec6b7
Justin Johnson <jjustin634@gmail.com>
2020-04-05 20:14:52 -0500

    changing api to grab 7 items from database to display to user on home page

--- client/src/components/home/homeApi.js
+++ client/src/components/home/homeApi.js
@@ -1,5 +1,5 @@
 export const getProducts = (sortBy) => {
-    return fetch(`/api/products?sortBy=${sortBy}&order=desc&limit=6`, {
+    return fetch(`/api/products?sortBy=${sortBy}&order=desc&limit=7`, {
       method: "GET",
     }).then(result => {
       return result.json()
